50d0d891d82569c834cc2e3efaf51c315378eaa6
Harshaa S <harshaas21@gmail.com>
2026-03-04 18:58:53 +0530

    Initial Commit

---
+++ merge_sort_with_2_fun.py
@@ -0,0 +1,19 @@
+def merge(a,b):
+    (C, m, n) = ([], len(a), len(b))
+    (i, j) = (0, 0)
+
+    while i+j < m+n:
+        if i == m:
+            C.append(b[j])
+            j=j+1
+        elif j == n:
+            C.append(a[i])
+            i=i+1
+        elif a[i] <= b[j]:
+            C.append(a[i])
+            i=i+1
+        elif a[i] > b[j]:
+            C.append(b[j])
+            j=j+1
+    return(C)
+
